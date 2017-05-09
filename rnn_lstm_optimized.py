15f5ebe617564036bb279125300c556f623c051f
ayanavasarkar <f2013206@dubai.bits-pilani.ac.in>
2017-05-09 13:09:49 +0200

    Results of iterations added

--- rnn_lstm_optimized.py
+++ rnn_lstm_optimized.py
@@ -220,7 +220,7 @@ s= "Batch size = "+str(batch_size)+"---"+ "Hidden Layers = "+str(n_layers)+"---"
 
 dic[s] = ((float(accuracy_counter))/(float(n_test)) *float(100))
 
-#with open('results.json', 'w') as f:
-#		json.dump(dic, f)
+with open('results.json', 'w') as f:
+		json.dump(dic, f)
 
 print (time.time()-start)
